61e421a39492c721d6f7623840f89cbb243584f7
julio.quiroga <quirogajulio27@gmail.com>
2018-01-17 16:42:47 -0300

    ruta9 la definitiva

--- retrato.php
+++ retrato.php
@@ -12,10 +12,10 @@
     <?php
     $path = '/images/Retratos/Varios';//.$rows['producto_id'];
     $carpeta = $_SERVER['DOCUMENT_ROOT'] . '/' . $path;
-    echo "ruta1" .$carpeta;
+
     if($dir = opendir($carpeta)){
       while(($archivo = readdir($dir)) !== false) {
-        echo "ruta" .$archivo;
+
         if($archivo != '.' && $archivo != '..'){ ?>
 
             <div class="grid-item">
