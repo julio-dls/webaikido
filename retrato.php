eea2e8979b731d7dee142897009f7c1ab6d2b39c
julio.quiroga <quirogajulio27@gmail.com>
2018-01-17 16:24:59 -0300

    ruta3

--- retrato.php
+++ retrato.php
@@ -10,7 +10,7 @@
     <hr>
     <div class="grid">
     <?php
-    $path = 'images\Retratos\Varios';//.$rows['producto_id'];
+    $path = 'images/Retratos/Varios';//.$rows['producto_id'];
     $carpeta = $_SERVER['DOCUMENT_ROOT'] . '/' . $path;
     echo "ruta" .$carpeta;
     if($dir = opendir($carpeta)){
