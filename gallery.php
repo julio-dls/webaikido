944a55ef6e0c166483afe6388e81decfcf4356f2
julio.quiroga <quirogajulio27@gmail.com>
2018-01-18 20:07:04 -0300

    solucionando error de categorias poses17

--- gallery.php
+++ gallery.php
@@ -17,7 +17,7 @@
       $categoria = $_GET['categoria'];
     }
 
-    $path = 'Web_Aikido/images/' .$categoria;//'.$rows['producto_id'];'images/'.$categoria;//
+    $path = 'images/'.$categoria;//'Web_Aikido/images/' .$categoria;//'.$rows['producto_id'];
     trim($path);
     $carpeta = $_SERVER['DOCUMENT_ROOT'] . '/' .$path;
     $ficheros1  = scandir($carpeta);
